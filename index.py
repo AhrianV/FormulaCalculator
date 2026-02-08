394bc773e0029e3f4a599804d55b8cd6289da6a3
Ahrian <167486328+AhrianV@users.noreply.github.com>
2026-02-08 14:18:54 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-response = int(input("list of numbers to plug in as d value of fx = 2.9x/100:" ))
+response = input("list of numbers to plug in as d value of fx = 2.9x/100:" ))
+nums = response.split()
+for num of nums:
+	x = float(num)
+    y = (2.9 * x) / 100
+    y_val = str(y)
+    print(f"Input: {num} Output: {y_val}")
